91ff784aa3e5a5802ff400b458168f47fdf92511
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-08-11 12:16:25 +0300

    branch 'max' added

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
-        List<Thread> threads = new ArrayList<>();
+        List<Future<Integer>> futures = new ArrayList<>();
 
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
-            Runnable logic = () -> {
+            Callable task = () -> {
                 int maxSize = 0;
                 for (int i = 0; i < text.length(); i++) {
                     for (int j = 0; j < text.length(); j++) {
@@ -31,28 +35,23 @@ public class Main {
                     }
                 }
                 System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                return maxSize;
             };
-            threads.add(new Thread(logic));
+            FutureTask<Integer> future = new FutureTask<>(task);
+            futures.add(future);
+            new Thread(future).start();
         }
 
-        for (Thread thread : threads) {
-            thread.start();
+        int maxSize = 0;
+
+        for (Future<Integer> future : futures) {
+//            new Thread(String.valueOf(future)).start();
+            if (future.get() > maxSize) {
+                maxSize = future.get();
+            }
         }
-
-//        for (Thread thread : threads) {
-//            try {
-//                thread.join();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
-        }
-
+        System.out.println("Максимальный интервал значений: " + maxSize);
         long endTs = System.currentTimeMillis(); // end time
-
         System.out.println("Time: " + (endTs - startTs) + "ms");
     }
 
